49fe99e194de09bae31a72fda084d997f3e4dcad
Angus Macrae <angusdmacrae@gmail.com>
2020-05-02 23:19:23 +0800

    Styling improvements, continued work on ajax page loading functionality

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-
-
-document.getElementById("about-ajax").onclick = function () {
-
+function showPage(newPage) {
+    
     $("#content-wrapper").fadeOut(250, function () {
 
-        $("#content-wrapper").load("index.html #content", function () {
+        $("#content-wrapper").load(newPage + ".html #content", function () {
 
             $("#content-wrapper").fadeIn(250);
 
         });
 
     });
-    history.pushState("about", null, "about");
+    
+    history.pushState(newPage, null, newPage);
+    
+}
 
-};
+$("#about-nav").click(function(e) {
+    
+    e.preventDefault();
+    showPage("about");
+//    $(".nav-link").toggleClass("active-page")
+    $("#about-nav").addClass("active-page");
+    $("#portfolio-nav").removeClass("active-page");
+    $("#contact-nav").removeClass("active-page");
+    
+});
 
-document.getElementById("portfolio-ajax").onclick = function () {
+$("#portfolio-nav").click(function(e) {
+    
+    e.preventDefault();
+    showPage("portfolio");
+    $("#about-nav").removeClass("active-page");
+    $("#portfolio-nav").addClass("active-page");
+    $("#contact-nav").removeClass("active-page");
+    
+});
 
-    $("#content-wrapper").fadeOut(250, function () {
+$("#contact-nav").click(function(e) {
+    
+    e.preventDefault();
+    showPage("contact");
+    $("#about-nav").removeClass("active-page");
+    $("#portfolio-nav").removeClass("active-page");
+    $("#contact-nav").addClass("active-page");
+    
+});
 
-        $("#content-wrapper").load("portfolio.html #content", function () {
-
-            $("#content-wrapper").fadeIn(250);
-
-        });
-
-    });
-    history.pushState("portfolio", null, "portfolio");
-
-};
-
-document.getElementById("contact-ajax").onclick = function () {
-
-    $("#content-wrapper").fadeOut(250, function () {
-
-        $("#content-wrapper").load("contact.html #content", function () {
-
-            $("#content-wrapper").fadeIn(250);
-
-        });
-
-    });
-    history.pushState("contact", null, "contact");
-
-};
-
-//window.onpopstate = function(e) {
-//    
-//    if (e.state != null) {
-//        
-//        
-//        
-//    }
-//    
-//}
+window.onpopstate = function(e) {
+    
+    if (e.state != null) {
+        
+        showPage(e.state);
+        
+    }
+    
+}
