5a30e7a0c5ba91483f6d3b5d314ca19f1b78ad15
Angus Macrae <angusdmacrae@gmail.com>
2020-05-03 11:52:36 +0800

    Added htaccess file to hand url, optimised js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function showPage(newPage, push = true) {
     
-    $("#content-wrapper").fadeOut(250, function () {
+    $("#content-wrapper").fadeOut(200, function () {
 
         $("#content-wrapper").load(newPage + ".html #content", function () {
 
-            $("#content-wrapper").fadeIn(250);
+            $("#content-wrapper").fadeIn(200);
 
         });
 
@@ -16,37 +16,25 @@ function showPage(newPage, push = true) {
         
     }
 
-    $("#about-nav").removeClass("active-page");
-    $("#portfolio-nav").removeClass("active-page");
-    $("#contact-nav").removeClass("active-page");
+    $(".nav-link").removeClass("active-page");
     $("#" + newPage +"-nav").addClass("active-page");
     
 }
 
-$("#about-nav").click(function(e) {
+$(".nav-link").click(function(e) {
     
     e.preventDefault();
-    showPage("about");
-    
-});
-
-$("#portfolio-nav").click(function(e) {
-    
-    e.preventDefault();
-    showPage("portfolio");
-    
-});
-
-$("#contact-nav").click(function(e) {
-    
-    e.preventDefault();
-    showPage("contact");
+    showPage($(this).attr("id").slice(0,-4));
     
 });
 
 window.onpopstate = function(e) {
     
-    if (e.state != null) {
+    if (e.state == null) {
+    
+        showPage("about", false);
+        
+    } else {
         
         showPage(e.state, false);
         
