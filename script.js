bd26433e26585c0c828b0f19da05c5b7ea02c857
Angus Macrae <angusdmacrae@gmail.com>
2020-05-21 11:41:02 +0800

    Add animated loading screen

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const sectionMargin = 200;
 
     const makeActive = (link) => links[link].classList.add("active-page");
-
     const removeActive = (link) => links[link].classList.remove("active-page");
-
     const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+    function fadeLoading() {
+        document.querySelector('.loading-screen').classList.add("faded-out");
+        document.querySelector('.socket').classList.add("faded-out");
+    }
+
+    function enableScroll() {
+        document.querySelector('body').classList.add("visible");
+    }
+
+    setTimeout(fadeLoading, 500);
+    setTimeout(enableScroll, 3500);
 
     // let currentActive = 0;
 
